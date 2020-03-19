350b2d4b9d1c5d0cc133975cebe0d1c5e554286b
Ali SaIT Karamanli <55097581+alisahp@users.noreply.github.com>
2020-03-19 16:22:10 -0500

    Update google_bucket.tf

--- kube-cluster/google_bucket.tf
+++ kube-cluster/google_bucket.tf
@@ -1,14 +1,5 @@
-resource "google_storage_bucket" "mybestsea-auto-expire" {
-  name          = "auto-expiring-bucket"
+resource "google_storage_bucket" "mybestsea2023_bucket" {
+  name          = "var.google_bucket_name"
+  storage_class = "COLDLINE"
   location      = "US"
-  force_destroy = true
-
-  lifecycle_rule {
-    condition {
-      age = "7"
-    }
-    action {
-      type = "Delete"
-    }
-  }
 }
