51fc009ab475e7d088ce411d472cc8a8fd04df40
Ali SaIT Karamanli <55097581+alisahp@users.noreply.github.com>
2020-03-18 19:56:25 -0500

    Update google_bucket.tf

--- kube-cluster/google_bucket.tf
+++ kube-cluster/google_bucket.tf
@@ -1,4 +1,4 @@
-resource "google_storage_bucket" "mybestsea1" {
+resource "google_storage_bucket" "mybestsea2" {
   name          = "${var.google_bucket_name}"
   storage_class = "COLDLINE"
   location      = "${var.region}"
