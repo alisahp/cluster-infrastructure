1efd1c58560a07e5231da55b9768fa2ff48778c5
Ali SaIT Karamanli <55097581+alisahp@users.noreply.github.com>
2020-03-19 17:01:52 -0500

    Update kubecluster-deployment.tf

--- kube-cluster/kubecluster-deployment.tf
+++ kube-cluster/kubecluster-deployment.tf
@@ -2,17 +2,14 @@ provider "google" {
   credentials = "${file("./mybestsea-service-account.json")}" #GOOGLE_CREDENTIALS to the path of a file containing the credential JSON
   project     = "${var.google_project_id}"
 }
-
 resource "google_container_cluster" "mybestsea" {
   name               = "mybestsea"
   network            = "default"
   subnetwork         = "default"
-  region             = "us-central1"
   location           = "us-central1-a"
   min_master_version = "${var.cluster_version}"
   initial_node_count = "${var.node_count}"
   project            = "${var.google_project_id}"
-
   node_config {
     machine_type = "n1-standard-2"
   }
