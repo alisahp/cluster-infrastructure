e8791989394b86a43516b250ba159a821792b492
Ali SaIT Karamanli <55097581+alisahp@users.noreply.github.com>
2020-03-22 12:21:06 -0500

    dns name alisait.net updated

--- kube-cluster/google-zone.tf
+++ kube-cluster/google-zone.tf
@@ -1,5 +1,5 @@
 resource "google_dns_managed_zone" "mybestsea" {
   name     = "mybestsea"
-  dns_name = "alisait.net."
+  dns_name = "alisait.net."   #dns name alisait.net updated
   project  = "${var.google_project_id}"
 }
